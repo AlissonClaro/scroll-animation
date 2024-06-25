b2a478a9420b2f9be8f6e586409462c0abca41f7
AlissonClaro <allnclaro@outlook.com>
2024-06-25 13:15:30 -0300

    edit scrolltriger

--- script.js
+++ script.js
@@ -9,12 +9,19 @@ const scroller = new LocomotiveScroll ({
     scroller.on('scroll', ScrollTrigger.update)
     ScrollTrigger.scrollerProxy(pageContainer, {
         scrollTop(value) {
-            return arguments.length ? scroller.scrollTop(value, 0, 0) : scroller.scroll.instance.scroll.y
+            return arguments.length
+            ? scroller.scrollTop(value, 0, 0)
+            : scroller.scroll.instance.scroll.y
         },
         getBoundingClientRect() {
-            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight, }
+            return { 
+                top: 0, 
+                left: 0, 
+                width: window.innerWidth, 
+                height: window.innerHeight, 
+            }
         },
-        pinType: pageContainer.computedStyleMap.transform ? 'transform' : 'fixed'
+        pinType: pageContainer.style.transform ? 'transform' : 'fixed'
     })
 
     window.addEventListener('load', () => {
@@ -25,18 +32,17 @@ const scroller = new LocomotiveScroll ({
         gsap.to('.pin-wrap', {
             ScrollTrigger: {
                 scroller: pageContainer,
+                scrub: true,
                 trigger: '.pin-wrap',
+                pin: true,
                 start: 'top top',
                 end: pinWrapWidth,
-                scrub: true,
-                pin: true,
-                
             },
             x: -horizontalScrollLength
     })
 
     ScrollTrigger.addEventListener('refresh', () => scroller.update())
-    ScrollTrigger.refresh()
+    ScrollTrigger.refresh();
     })
 
 
